13b34a1c9dac885cd6b035075b9fa5d0e69984f5
kamilakhairunnisa <erinazkk@gmail.com>
2025-11-13 17:00:17 +0700

    Tugas array 2

--- Tugas08.java
+++ Tugas08.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class Tugas08 {
-    public static void main(String[] args) {
+    public static void main(String[] args){
        Scanner sc= new Scanner (System.in); 
     
         int responden = 10;
